5ab84a5d26d482a57dca1d901df02877e196a12b
Art Nattapat <nattapat.art@gmail.com>
2017-10-11 19:48:52 +0700

    feat(server.connectors.user): connected with nap

--- connectors/user.ts
+++ connectors/user.ts
@@ -1,35 +1,78 @@
 import * as mongoose from 'mongoose'
+import ApolloClient from 'apollo-client'
+import Link from 'apollo-link-http'
+import Cache from 'apollo-cache-inmemory'
+import gql from 'graphql-tag'
+
+const UserInfoResolver = gql`
+  query UserInfoResolver($userId: String!){
+    resolveUserInfo(_id: $userId) {
+      _id
+      name
+      profilePictureURL
+    }
+  }
+`
+
+const getUserIdFromTokenQuery = gql`
+  query getUserIdFromToken($token: String!){
+    getUserIdFromToken(token: $token)
+  }
+`
 
 export type GQUserConnector = {
   resolveUserInfo: (userId: string) => Promise<GBUserType>
   getUserIdFromToken: (token: string) => Promise<string>
 }
 
-export default (config: { logger: ApplicationLogger, endpoint: string, userModel: mongoose.Model<GQUserDocument> }): GQUserConnector => ({
-  async resolveUserInfo(userId) {
-    const user = await Promise.resolve({
-      name: 'mock',
-      profileImageURL: null,
-      _id: userId
-    })
-    if (user && user._id) {
-      config.userModel.findOneAndUpdate({
-        _id: user._id
-      }, {
-        name: user.name,
-        profileImageURL: user.profileImageURL
-      }, {
-        upsert: true
-      }).exec().catch((error) => {
-        config.logger.log(`cant update user [${userId}] with message error: ${error.message}`)
+export default (config: { logger: ApplicationLogger, endpoint: string, userModel: mongoose.Model<GQUserDocument> }): GQUserConnector => {
+  const nap = new ApolloClient(({
+    link: new Link({
+      uri: config.endpoint
+    }),
+    cache: new Cache({})
+  }) as any)
+  return {
+    async resolveUserInfo(userId) {
+      if (!userId) {
+        return null
+      }
+      const user = await nap.query<{resolveUserInfo: GBUserType}>({
+        query: UserInfoResolver,
+        variables: { userId }
       })
+        .then((result) => result.data ? result.data.resolveUserInfo : null)
+        .catch((error) => {
+          config.logger.log(`nap error: ${error.message}`)
+          return null
+        })
+      if (user && user._id) {
+        config.userModel.findOneAndUpdate({
+          _id: userId
+        }, {
+          name: user.name,
+          profilePictureURL: user.profilePictureURL
+        }, {
+          upsert: true
+        }).exec().catch((error) => {
+          config.logger.log(`cant update user [${userId}] with message error: ${error.message}`)
+        })
+      }
+      if (!user) {
+        return config.userModel.findById(userId)
+      }
+      return user
+    },
+    getUserIdFromToken(token) {
+      return nap.query<{getUserIdFromToken: string}>({
+        query: getUserIdFromTokenQuery,
+        variables: { token }
+      })
+        .then((result) => result.data ? result.data.getUserIdFromToken : null)
+        .catch((error) => {
+          config.logger.log(`nap error: ${error.message}`)
+          return null
+        })
     }
-    if (!user) {
-      return config.userModel.findById(userId)
-    }
-    return user
-  },
-  getUserIdFromToken(token) {
-    return Promise.resolve('mock-user-id')
   }
-})
+}
