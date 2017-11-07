76905fb65b22d412a78e168dd0601b58ee142d5d
Rungsikorn Rungsikavanich <rungsikorn@me.com>
2017-11-07 19:40:46 +0700

    add deployment target

--- .bin/deliver.bash
+++
@@ -1,4 +0,0 @@
-export $(cat .env | grep -v ^# | xargs)
-rsync -avz ./dist/artifact.tar $DEPLOY_TARGET:/opt/comment-service-artifact.tar
-ssh -t $DEPLOY_TARGET 'cd /opt && ls && mkdir -p jamplay-comment-service && tar -xvf comment-service-artifact.tar -C /opt/jamplay-comment-service'
-
