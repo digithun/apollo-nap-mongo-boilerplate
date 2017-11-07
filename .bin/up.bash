c82cc2f1021fba1c83dfb0c5f2b7bf963bd3de13
Rungsikorn Rungsikavanich <rungsikorn@me.com>
2017-11-07 19:57:30 +0700

    update up script

--- .bin/up.bash
+++ .bin/up.bash
@@ -1,10 +1,8 @@
-export $(cat .env | grep -v ^# | xargs)
-
-ssh -t $DEPLOY_TARGET 'cd /opt/jamplay-comment-service&&pm2 describe jamplay-comment > /dev/null
+cd /opt/jamplay-comment-service&&pm2 describe jamplay-comment > /dev/null
 RUNNING=$?
 
 if [ "${RUNNING}" -ne 0 ]; then
   pm2 start ./pm2.prod.json
 else
   pm2 restart jamplay-comment
-fi;'
+fi;
