bec130b808e32748eed3abb566e5d32c5d97d8cb
Rungsikorn Rungsikavanich <rungsikorn@icloud.com>
2017-11-15 13:51:23 +0700

    update build script

--- .bin/build.bash
+++ .bin/build.bash
@@ -1,7 +1,7 @@
 export $(cat .env | grep -v ^# | xargs)
 yarn clean
 yarn tsc
-yarn next -- build
+NODE_ENV=production yarn next -- build
 rm -rf ./dist
 mkdir -p dist
 tar --exclude='.git' --exclude='.db' --exclude='.env' --exclude='./dist' --exclude='./dist/artifact.tar' --exclude='./node_modules' --exclude='./.vscode' -czf ./dist/artifact.tar ./
