9f9d9f76b334d9f2b935dbf9a2a2c8e6eb4df0fc
Rungsikorn Rungsikavanich <rungsikorn@icloud.com>
2017-11-15 16:25:13 +0700

    add uglify on prod

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,14 @@
+const Uglify = require('uglifyjs-webpack-plugin')
 module.exports = {
   webpack(cfg) {
-    cfg.plugins = cfg.plugins.filter((plugin) => {
-      if (plugin.constructor.name === 'UglifyJsPlugin') {
-        return false;
-      } else {
-        return true;
-      }
-    });
+      cfg.plugins = cfg.plugins.filter(
+        (plugin) => (plugin.constructor.name !== 'UglifyJsPlugin')
+      )
+      cfg.plugins.push(
+        new Uglify({
+          sourceMap: false
+        })
+      )
 
     return cfg;
   }
