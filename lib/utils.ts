97399caa2bd98b12660d689b7e9b38dc39b9b995
Art Nattapat <nattapat.art@gmail.com>
2017-11-23 15:26:50 +0700

    fix(lib.utils): use console.log instead of chalk

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,3 @@
-const chalk = require('chalk')
-
 export function requiredEnv(name: any) {
   if (process.env[name]) {
     return process.env[name]
@@ -12,7 +10,7 @@ export function optionalEnvWithDefault(name: any, defaultValue: any) {
   if (process.env[name]) {
     return process.env[name]
   } else {
-    console.log(chalk.yellow(`${name} not defined use ${defaultValue}`))
+    console.log(`${name} not defined use ${defaultValue}`)
     return defaultValue
   }
 }
