cf763d2e794d49fed128f373cb68250327ada539
Rungsikorn Rungsikavanich <rungsikorn@icloud.com>
2017-12-06 15:38:40 +0700

    date babel style component versin

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,14 @@
 const Uglify = require('uglifyjs-webpack-plugin')
 module.exports = {
   webpack(cfg) {
-      cfg.plugins = cfg.plugins.filter(
-        (plugin) => (plugin.constructor.name !== 'UglifyJsPlugin')
-      )
-      cfg.plugins.push(
-        new Uglify({
-          sourceMap: false
-        })
-      )
+    cfg.plugins = cfg.plugins.filter(
+      (plugin) => (plugin.constructor.name !== 'UglifyJsPlugin')
+    )
+    cfg.plugins.push(
+      new Uglify({
+        sourceMap: false
+      })
+    )
 
     return cfg;
   }
